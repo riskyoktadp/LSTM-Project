33ad086b28381caf68229b0b5c2da5ea8fe0b346
Risky Okta Dwi Putra <103984245+riskyoktadp@users.noreply.github.com>
2022-12-27 23:47:55 +0700

    Add files via upload

---
+++ 4 End Point/LSTM (2).py
@@ -0,0 +1,116 @@
+import re, pickle
+
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+from flask import request
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+
+
+import numpy as np
+
+
+app.json_encoder = LazyJSONEncoder
+swagger_template = dict(
+    info = {
+        'title': LazyString(lambda: 'API Documentation for Data Processing and Modeling'),
+        'version': LazyString(lambda: '1.0.0'),
+        'description': LazyString(lambda: 'Dokumentasi API untuk Data Processing dan Modeling')
+    },
+    host = LazyString(lambda: request.host)
+)
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'docs',
+            "route": '/docs.json'
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/docs/"
+}
+swagger = Swagger(app, template=swagger_template,config=swagger_config)
+
+def cleansing(sent):
+    # Mengubah kata menjadi huruf kecil semua dengan menggunakan fungsi lower()
+    string = sent.lower()
+    # Menghapus emoticon dan tanda baca menggunakan "RegEx" dengan script di bawah
+    string = re.sub(r'[^a-zA-Z0-9]', ' ', string)
+    return string
+
+sentiment = ['negatif', 'netral', 'positif']
+
+file_tokenizer = open('tokenizer.pickle', 'rb')
+# max_features = 100000
+# tokenizer = Tokenizer(num_words=max_features, split=' ', lower=True)
+file_sequencer = open('x_pad_sequences.pickle', 'rb')
+
+load_tokenizer = pickle.load(file_tokenizer)
+load_sequencer = pickle.load(file_sequencer)
+file_sequencer.close()
+model_lstm = load_model('model_sentiment.h5')
+
+@app.route('/')
+def hello_world():
+    return("Hello World")
+
+@swag_from("docs/lstm.yml", methods=['POST'])
+@app.route('/lstm', methods=['POST'])
+def lstm():
+
+    text = request.form.get('text')
+    cleanse_text = [cleansing(text)]
+
+
+    feature = Tokenizer.texts_to_sequences(cleanse_text)
+    feature = load_tokenizer.texts_to_sequences(cleanse_text)
+    feature = pad_sequences(feature, maxlen=load_sequencer.shape[1])
+
+    prediction = model_lstm.predict(feature)
+    get_sentiment = sentiment[np.argmax(prediction[0])]
+
+    json_response = {
+        'status_code': 200,
+        'description': text,
+        'sentiment': get_sentiment
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+@app.route('/data',methods=["POST"])
+def data_clean():
+    """upload files using cleaning data with the post method.
+    ---
+    tags:
+      - name: upload data file
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true  
+    responses:
+        200:
+            description: output values
+        400:
+            description: bad request
+        500: 
+            description: internal server error        
+        
+    """
+    df=pd.read_csv(request.files.get("file"))
+    print(df.head())
+    data=(df)
+
+    return str(data)    
+
+if __name__ == '__main__':
+    app.run(debug=True)
